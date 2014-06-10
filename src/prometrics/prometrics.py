b47c6fb8b03ed876fea78649e56ea0739877d1a9
Federico Figus <federico.figus@owasp.org>
2014-06-10 15:03:03 +0200

    add license and short description

--- src/prometrics/prometrics.py
+++ src/prometrics/prometrics.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python2.7 -OOBRtt
+# This file is part of Foobar.
+#
+#    OWASP Project Metrics is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with OWASP Project Metrics.  If not, see <http://www.gnu.org/licenses/>.
+"""Tool to measure project activity and quality"""
 from collections import defaultdict
 from datetime import datetime, timedelta
 import functools
@@ -14,7 +29,7 @@ from jinja2 import Environment, PackageLoader
 from namedlist import namedlist
 
 
-__doc__ = """Create a file with stas about a git repository.
+__doc__ = """Tool to measure project activity and quality.
 
 Usage: prometrics [--branches=<BRANCHES>]  [--output-format=<FMT>] [--path=<PATH>] [--project-name=<PROJ>] OUTPUT
 
