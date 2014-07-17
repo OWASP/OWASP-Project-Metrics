87cc7874bfc6436b4110d332be3e421734e7c06b
Federico Figus <figus.federico@gmail.com>
2014-07-17 23:36:27 +0200

    fix license

--- src/prometrics/core/__init__.py
+++ src/prometrics/core/__init__.py
@@ -1,4 +1,4 @@
-# This file is part of Foobar.
+# This file is part of WASP Project Metrics.
 #
 #    OWASP Project Metrics is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -13,4 +13,3 @@
 #    You should have received a copy of the GNU General Public License
 #    along with OWASP Project Metrics.  If not, see <http://www.gnu.org/licenses/>.
 from git import Git
-
