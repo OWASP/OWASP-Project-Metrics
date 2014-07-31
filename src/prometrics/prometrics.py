f71cc405e88f36012c540667e4a4efe9d6d3b4f9
Federico Figus <figus.federico@gmail.com>
2014-07-31 14:27:18 +0200

    add default options and add try..except for branches listing

--- src/prometrics/prometrics.py
+++ src/prometrics/prometrics.py
@@ -3,13 +3,13 @@ __doc__ = """Tool to measure project activity and quality.
 
 Usage:
     prometrics repo [--dir-out=<OUT>] [--repo-dir=<DIR>] [--output-format=<FMT>] [--name=<NAME>] <PATH>
-    prometrics list [--with-name] [--dir-out=<OUT>] [--repo-dir=<DIR>] [--output-format=<FMT>] <PATH>
+    prometrics list [--without-name] [--dir-out=<OUT>] [--repo-dir=<DIR>] [--output-format=<FMT>] [<PATH>]
 
 Options:
     -d --dir-out=<OUT>          output directory [default: ./]
     -f --output-format=<FMT>    output-format [default: html5]
     --repo-dir=<DIR>            directory where store repository [default: /tmp/]
-    --with-name
+    --without-name
 """
 
 from collections import namedtuple
@@ -21,6 +21,7 @@ import random
 import shutil
 import string
 import tempfile
+import time
 
 from docopt import docopt
 
@@ -41,7 +42,7 @@ def log(msg):
 
 
 def prepare_fname(name):
-    return ''.join(ch.upper() for ch in name if ch.isalnum() or ch in '_ ').replace(' ', '_')
+    return ''.join(ch.upper() for ch in name if ch.isalnum() or ch in '_. ').replace(' ', '_').replace('.', '_')
 
 
 def random_string(n):
@@ -57,29 +58,25 @@ def launch(reporter, dir_tmp, dir_out, name, path):
         except GitCmdError, ex:
             log("ERROR: repository %r cloning repository : %r" % (name, str(ex)))
             raise
-        try:
-            # get info
-            proj_out = os.path.join(dir_out, prepare_fname(name))
-            if not os.path.exists(proj_out):
-                os.mkdir(proj_out)
-            for branch in repo.branches():
-                try:
-                    repo.branch = branch
-                except GitCmdError, ex:
-                    log("ERROR: repository %r switching to branch %r : %r" % (name, branch, str(ex)))
-                    continue
-                branch_out = os.path.join(proj_out, prepare_fname(branch))
-                if not os.path.exists(branch_out):
-                    os.mkdir(branch_out)
-                log("reporting %r:%s" % (name, branch))
-                try:
-                    reporter(repo, dirpath=proj_out, project_name=name)
-                except repout.ReportError, ex:
-                    log("ERROR: repository %r reporting branch %r : %r" % (name, branch, str(ex)))
-                    continue
-        finally:
-            log("removing repository %r at %r" % (name, repo.path))
-            shutil.rmtree(repo.path)
+        # get info
+        proj_out = os.path.join(dir_out, prepare_fname(name))
+        if not os.path.exists(proj_out):
+            os.mkdir(proj_out)
+        for branch in repo.branches():
+            try:
+                repo.branch = branch
+            except GitCmdError, ex:
+                log("ERROR: repository %r switching to branch %r : %r" % (name, branch, str(ex)))
+                continue
+            branch_out = os.path.join(proj_out, prepare_fname(branch))
+            if not os.path.exists(branch_out):
+                os.mkdir(branch_out)
+            log("reporting %r:%s" % (name, branch))
+            try:
+                reporter(repo, dirpath=proj_out, project_name=name)
+            except repout.ReportError, ex:
+                log("ERROR: repository %r reporting branch %r : %r" % (name, branch, str(ex)))
+                continue
     except KeyboardInterrupt:
         pass
     except (GitCmdError, repout.ReportError), ex:
@@ -108,20 +105,20 @@ def _prometrics():
     elif opts['list']:
         queue = []
         procs = {}
-        index = []
-        with open(opts['<PATH>'], 'rb') as fl:
-            if opts['--with-name']:
+        index = {}
+        with open(opts['<PATH>'] or './OWASP_public_repo_list.txt', 'rb') as fl:
+            if opts['--without-name']:
+                for line in fl:
+                    name = os.path.split(opts['<PATH>'])[1]
+                    if name.endswith('.git'):
+                        name = name[:-4]
+                    queue.append((name, path))
+            else:
                 for line in fl:
                     line = line.strip()
                     if not line:
                         continue
                     name, _, path = line.strip().partition(';')
-                    queue.append((name, path))
-            else:
-                for line in fl:
-                    name = os.path.split(opts['<PATH>'])[1]
-                    if name.endswith('.git'):
-                        name = name[:-4]
                     queue.append((name, path))
         while queue or procs:
             if queue and len(procs) < MAX_JOBS:
@@ -131,15 +128,24 @@ def _prometrics():
                 p = multip.Process(target=launch, args=args)
                 p.start()
                 procs[p.pid] = p, name
-                index.append([name, tmp_out, path, os.path.join(dirout, prepare_fname(name))])
+                index[name] = name, tmp_out, path, os.path.join(dirout, prepare_fname(name))
+                time.sleep(3)
             else:
                 for pid, (proc, name) in procs.items():
                     if not proc.is_alive():
                         procs.pop(pid, None)
+                        name, tmp_out, origin, path = index[name]
+                        try:
+                            index[name] = name, core.Git(tmp_out).branches(), origin, path
+                        except GitCmdError, ex:
+                            log("ERROR: opening repository %r to read branches: %r" % (name, str(ex)))
+                            del index[name]
                         log("end repository %r" % name)
-        for l in index:
-            l[1] = core.Git(l[1])
-        index_reporter(dirout, index)
+        try:
+            log("printing main index...")
+            index_reporter(dirout, index.values())
+        except repout.ReportError, ex:
+            log("ERROR: printing main index: %r" % str(ex))
     else:
         return
 
@@ -151,3 +157,4 @@ if __name__ == '__main__':
         log("SIGINT interruption")
     except Exception, ex:
         log("Error: %r" % str(ex))
+
